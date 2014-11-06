1a1853dcb6a16e1e83eb7aeebbc2799920960d9e
Sindre <sinstavse@gmail.com>
2014-11-06 16:22:53 +0100

    Added code

--- Ex1/mytagger.py
+++ Ex1/mytagger.py
@@ -1,17 +1,43 @@
 import sys
 
-infile = sys.argv[1]
-
 word_dict = dict()
+unigram_dict = dict()
 
-with open( infile , 'r' ) as input:
-    while input.readline():
-        line = input.readline()
-        if line: 
-            inst = line.strip().split(" ")
-            if inst[1] == "WORDTAG":
-                print float(inst[0]), inst[2], inst[3]
-                word_dict[inst[3]] = (float(inst[0]), inst[2])
-            
+def group_counts( infile ):
+    with open( infile , 'r' ) as input:
+        for line in input:
+            #print line
+            if line: 
+                inst = line.strip().split(" ")
+                #Get all word counts
+                cnt = float(inst[0])
+                tag = inst[2]
+                if inst[1] == "WORDTAG":
+                    word = inst[3]
+                    tag_count_pairs = word_dict.get( word, [] )
+                    tag_count_pairs.append( [ tag, cnt ] )
+                    word_dict[ word ] = tag_count_pairs
+                    #Get all unigram counts
+                elif inst[1] == "1-GRAM":
+                    unigram_dict[ tag ] = cnt
 
-print [ w[0] for w in word_dict.items() ]
+def calculate_ratios():
+    for word, tag_counts in word_dict.items():
+        tag_ratios = list()
+        for tag, cnt in tag_counts:
+            print word, tag, cnt
+            unigram_count = unigram_dict[ tag ]
+            tag_ratios.append( [tag, cnt/unigram_count ] ) 
+        word_dict[ word ] = tag_ratios
+
+if __name__ == "__main__":
+    
+    group_counts( sys.argv[1] )
+
+    calculate_ratios()
+
+    #print [ w for w in word_dict ]
+    print word_dict["is"]
+    
+    print unigram_dict.items()
+
