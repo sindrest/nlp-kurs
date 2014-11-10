6550ad440169b40867af101462830d8c865c42ae
Sindre <sinstavse@gmail.com>
2014-11-10 14:27:30 +0100

    Added function for replacing rare words

--- Ex1/mytagger.py
+++ Ex1/mytagger.py
@@ -1,6 +1,7 @@
 import sys
 
 word_dict = dict()
+word_count_dict = dict()
 unigram_dict = dict()
 
 def group_counts( infile ):
@@ -24,20 +25,44 @@ def group_counts( infile ):
 def calculate_ratios():
     for word, tag_counts in word_dict.items():
         tag_ratios = list()
+        word_count = 0
         for tag, cnt in tag_counts:
-            print word, tag, cnt
+            #print word, tag, cnt
             unigram_count = unigram_dict[ tag ]
+            word_count += cnt
             tag_ratios.append( [tag, cnt/unigram_count ] ) 
         word_dict[ word ] = tag_ratios
+        word_count_dict[ word ] = word_count
+
+def replace_rare_words( infile ):
+    
+    outfile = open("gene.train_mod", "w") 
+
+    with open( infile ) as input:
+        for line in input:
+            if line.strip():
+                if word_count_dict.get(line.strip().split(" ")[0],0) < 5:
+                    print line.strip(), word_count_dict.get(line.strip().split(" ")[0],0), "_RARE_"
+                    outfile.write( "_RARE_ " + line.strip().split(" ")[1] + "\n" )
+                else:
+                    outfile.write( line )
+            else:
+                outfile.write( line )
+                    
+    outfile.close()
 
 if __name__ == "__main__":
     
     group_counts( sys.argv[1] )
-
+    
     calculate_ratios()
+
+    replace_rare_words( sys.argv[2] ) 
 
     #print [ w for w in word_dict ]
     print word_dict["is"]
     
+    print word_count_dict["is"]
+
     print unigram_dict.items()
 
